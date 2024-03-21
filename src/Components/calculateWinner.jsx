3350149cd8f2bebeffda9149a423b850086399a3
Abraham Sanchez <abraham_gto@hotmail.com>
2024-03-21 10:04:03 -0600

    removed check for dash

--- src/Components/calculateWinner.jsx
+++ src/Components/calculateWinner.jsx
@@ -11,9 +11,7 @@ export function calculateWinner(squares) {
   ];
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
-
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      if (a === "-") continue;
       return lines[i];
     }
   }
